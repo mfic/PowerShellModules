b0a92414c7c65c03eba04af624d8ad9fef352dee
Jan Quest <jan@questnet.eu>
2022-09-28 22:43:20 +0200

    add Get-AdRolesPermissions

---
+++ ActiveDirectory/Get-AdRolesPermissions/Get-AdRolesPermissions.psm1
@@ -0,0 +1,39 @@
+Function Get-AdRolesPermissions {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $True, ValueFromPipeline = $True, HelpMessage = "Input of group objects to be processed.")]
+        $group,
+        [Parameter(HelpMessage = "Choose if the data will be exportet or displayed.")]
+        $export = $True,
+        [Parameter(HelpMessage = "Define the path where the data will be exported to.")]
+        $path = ".\RolesPermissions.csv"
+    )
+
+    Begin {
+        $output = @()
+    }
+
+    Process {
+        $obj = @{}
+        $grouplist = @()
+
+        $memGroup = (Get-ADGroup –Identity $group –Properties Memberof).Memberof
+        $memGroup | ForEach-Object { $groupList += ($_ -split ",*..=")[1] }
+        $group = ($group -split ",*..=")[1]
+
+        $obj[$group] = $grouplist
+
+        $output += $obj
+    }
+
+    End {
+        $data = $output | Select-Object @{n = 'Roles'; e = { $_.Keys } }, @{n = 'Permissions'; e = { $_.Values } }
+            
+        if ($export) {
+            $data | Export-Csv -TypeInformation -Delimiter ";" -Path $path
+        }
+        else {
+            $data
+        }
+    }
+}
